4372ea5fc821ead2e55ad2ef52ad12eaa469eb20
Anthony Song <songzixing98@gmail.com>
2020-10-28 11:56:01 +0800

    Add Feature - update main

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
-"""The main execution script for this package for testing."""
-from my_gym_super_mario_bros._app.cli import main
-
-
+"""The main execution script for this package."""
+# set matplotlib to override default X11 environment
+import matplotlib
+matplotlib.use('Agg')
+# import the main entry point of the application
+from src.cli import main
 # execute the main entry point of the CLI
 main()
